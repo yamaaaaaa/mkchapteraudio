38cf793bee74bfb1a1818c7db1d054b8e3936452
Takamori Yamashiro <>
2019-05-30 09:25:56 +0900

    fix small bugs

--- mkchapteraudio.sh
+++ mkchapteraudio.sh
@@ -10,14 +10,17 @@ if [ ! -e $1 ]; then
   exit 1
 fi
 
-mkvmerge -o $2.mkv --split chapters:all $1
+ffmpeg -i $1 -vn -acodec copy tmp.mp4
+mkvmerge -o $2.mkv --split chapters:all tmp.mp4
 mkdir $2
-find . -type f -name "$2*" | xargs -I VAR ffmpeg -i VAR -vn -acodec copy ./$2/VAR.m4a
+find . -type f -name "$2*" | xargs -I VAR ffmpeg -i VAR -vn -ab 480k -ac 2 ./$2/VAR.m4a
 find ./$2 -type f -name "$2*" | gsed 's/.mkv.m4a//' | xargs -I {} mv {}.mkv.m4a {}.m4a
-
-find ./$2 -type f -name "$2*" | gsed 's/.mkv.m4a//' | xargs -I {} mv {}.mkv.m4a {}.m4a
-
-rm tmp.m4v
+rm tmp.mp4
 find . -depth 1 -type f | grep './*mkv' | xargs rm
 
-find ./$2 -type f -name "$2*" | xargs -I {} mp4tags -A "$2" -a "$3" -albumartist "$3" {}
+count=0
+files=`find ./$2 -type f -name "$2*" | sort`
+for f in $files; do
+	count=`expr $count + 1`	
+	mp4tags -A "$2" -a "$3" -t $count -albumartist "$3" $f
+done
